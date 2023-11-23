10be92cf1a440ab57a50ed416aa191e2ac20741d
Jose Villarreal <jvillrr@amazon.com>
2023-11-23 15:13:29 -0800

    Exercise 1.6

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,61 +1,29 @@
-const Header = (props) => {
-  console.log(props)
-  return (
-    <h1>{props.course}</h1>
-  )
-}
+import { useState } from 'react'
 
-const Content = (props) => {
-  console.log(props)
-  return (
-    <div>
-      <Part part={props.parts[0]}/>
-      <Part part={props.parts[1]}/>
-      <Part part={props.parts[2]}/>
-    </div>
-  )
-}
+const DisplayKeyValue = ({name, value}) => <div>{name} {value}</div>
 
-const Part = (props) => {
-  console.log(props)
-  return (
-    <p>
-      {props.part.name} {props.part.exercises}
-    </p>
-  )
-}
-
-const Total = (props) => {
-  console.log(props)
-  return (
-    <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
-  )
-}
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
   return (
     <div>
-      <Header course={course.name} />
-      <Content parts = {course.parts}/>
-      <Total parts = {course.parts}/>
+      <h1> give feedback</h1>
+      <Button handleClick={() => setGood(good + 1)} text="good" />
+      <Button handleClick={() => setNeutral(neutral +1 )} text="neutral" />
+      <Button handleClick={() => setBad(bad + 1)} text="bad" />
+      <h1> statistics</h1>
+      <DisplayKeyValue name = "good" value = {good} />
+      <DisplayKeyValue name = "neutral" value = {neutral} />
+      <DisplayKeyValue name = "bad" value = {bad} />
     </div>
   )
 }
