83293e155a3d5f90a1c9064a8c49a82c8cc8b608
Jose Villarreal <jvillrr@amazon.com>
2024-01-15 15:10:13 -0800

    Correction to exercise 2.11

--- part2/src/AppPhonebook.jsx
+++ part2/src/AppPhonebook.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
@@ -8,6 +9,16 @@ const AppPhonebook = () => {
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
+
+  useEffect(()=>{
+      console.log('Running effect hook')
+      axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
 
   const addPerson = (event) => {
     event.preventDefault()
