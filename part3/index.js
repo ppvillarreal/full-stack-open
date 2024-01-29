7241f4fe270702cfbca4644ad0649bb8c219e17c
Jose Villarreal <jvillrr@amazon.com>
2024-01-28 16:16:00 -0800

    Phonebook preliminary backend

--- part3/index.js
+++ part3/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json());
+
 let phonebook = [
     { 
       "id": 1,
@@ -24,6 +26,12 @@ let phonebook = [
     }
 ]
 
+const ID_LENGTH = 1000000000;
+
+const generateId = () => {
+    return Math.floor(Math.random() * ID_LENGTH) + 1;
+}
+
 app.get('/info', (request, response) => {
     const message = 
         `<p>Phonebook has info for ${phonebook.length} people</p>` +
@@ -33,6 +41,53 @@ app.get('/info', (request, response) => {
 
 app.get('/api/phonebook', (request, response) => {
   response.json(phonebook)
+})
+
+app.get('/api/phonebook/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const contact = phonebook.find(contact => contact.id === id)
+    
+    if (contact) {
+      response.json(contact)
+    } else {
+      response.status(404).end()
+    }
+  })
+
+app.delete('/api/phonebook/:id', (request, response) => {
+    const id = Number(request.params.id)
+    phonebook = phonebook.filter(contact => contact.id !== id)
+  
+    response.status(204).end()
+})
+  
+app.post('/api/phonebook', (request, response) => {
+    console.log(request.body)
+    const body = request.body
+    console.log(body)
+    if (!body.name) {
+      return response.status(400).json({ 
+        error: 'name missing' 
+      })
+    } else if (!body.number) {
+      return response.status(400).json({ 
+        error: 'number missing' 
+      })
+    } else if (phonebook.find(contact => contact.name === body.name)){
+       return response.status(400).json({ 
+         error: 'name already exists' 
+       })
+    }
+
+    const contact = {
+      name: body.name,
+      number: body.number,
+      id: generateId(),
+    }
+  
+    phonebook = phonebook.concat(contact)
+  
+    response.json(phonebook)
 })
 
 const PORT = 3001
